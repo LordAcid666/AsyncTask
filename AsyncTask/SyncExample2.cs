55d6624e440c763b3d6f7d21cefd6d1db5ba0298
Casalnovo Giacomo <gc76@live.it>
2023-03-17 16:15:19 +0100

    implemented methods

--- AsyncTask/SyncExample2.cs
+++ AsyncTask/SyncExample2.cs
@@ -2,5 +2,32 @@
 {
     public static class SyncExample2
     {
+        public static int IstruzioneA()
+        {
+            Console.WriteLine("A Start");
+            Thread.Sleep(4000);
+            Console.WriteLine("A End");
+
+            return 5;
+        }
+
+        public static int IstruzioneB()
+        {
+            Console.WriteLine("B Start");
+            Thread.Sleep(3000);
+            Console.WriteLine("B End");
+
+            return 7;
+        }
+
+        public static void IstruzioneC(int inputIstruzioneA, int inputIstruzioneB)
+        {
+            Console.WriteLine("C Start");
+            Thread.Sleep(2000);
+
+            int somma = inputIstruzioneA + inputIstruzioneB;
+
+            Console.WriteLine($"C End - Risultato: {somma}");
+        }
     }
 }
