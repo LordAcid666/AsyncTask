d15dc091d0d64e6079d936bf4634491e6e48b5cd
Casalnovo Giacomo <gc76@live.it>
2023-03-17 16:42:46 +0100

    add IstruzioneA()

--- AsyncTask/AsyncExample1.cs
+++ AsyncTask/AsyncExample1.cs
@@ -2,5 +2,11 @@
 {
     public static class AsyncExample1
     {
+        public static void IstruzioneA()
+        {
+            Console.WriteLine("A Start");
+            Thread.Sleep(4000);
+            Console.WriteLine("A End");
+        }
     }
 }
