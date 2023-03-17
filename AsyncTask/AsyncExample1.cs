4ca929280e73bccd6746e4b42c8a99f49a5d981e
Casalnovo Giacomo <gc76@live.it>
2023-03-17 16:46:55 +0100

    add: - AsyncIstruzioneB() - AsyncIstruzioneC()

--- AsyncTask/AsyncExample1.cs
+++ AsyncTask/AsyncExample1.cs
@@ -2,22 +2,34 @@
 {
     public static class AsyncExample1
     {
-        public static void IstruzioneA()
-        {
-            Console.WriteLine("A Start");
-            Thread.Sleep(4000);
-            Console.WriteLine("A End");
-        }
-
         public static Task AsyncIstruzioneA()
         {
             return Task.Run(() =>
             {
                 Console.WriteLine("A Start");
-            Thread.Sleep(4000);
-            Console.WriteLine("A End");
+                Thread.Sleep(4000);
+                Console.WriteLine("A End");
             });
-            
+        }
+
+        public static Task AsyncIstruzioneB()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("B Start");
+                Thread.Sleep(3000);
+                Console.WriteLine("B End");
+            });
+        }
+
+        public static Task AsyncIstruzioneC()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("C Start");
+                Thread.Sleep(2000);
+                Console.WriteLine("C End");
+            });
         }
     }
 }
